069f339d475bad2ebb2db179053544287952ef17
Gilberto Martins <gilbertomartinspro@gmail.com>
2023-02-19 08:45:23 -0300

    Arquivos iniciais da especialização Java ONE

---
+++ TestaVariaveis.java
@@ -0,0 +1,22 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Ola novo teste");
+        
+        int idade;
+        idade = 35;
+        System.out.println("A idade e " + idade);
+
+        double salario = 5500.50;
+        System.out.println("O salario e: " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+
+        double novaTentativa = 5.0 / 2;
+        System.out.println(novaTentativa);
+        
+    }
+}
