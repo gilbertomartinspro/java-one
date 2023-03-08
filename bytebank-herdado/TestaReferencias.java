58d6e0d11861451bc03f915f771898558d995d3c
Gilberto Alves Martins <gilberto.martins@HINOVA.net>
2023-03-08 14:00:54 -0300

    Entendendo Polimorfismo

---
+++ bytebank-herdado/TestaReferencias.java
@@ -0,0 +1,26 @@
+public class TestaReferencias {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Gilberto");
+        g1.setSalario(5000.00);
+
+        Funcionario f1 = new Funcionario();
+        f1.setNome("João");
+        f1.setSalario(2500.00);
+
+        EditorVideo ev1 = new EditorVideo();
+        ev1.setSalario(3000.00);
+
+        Designer d1 = new Designer();
+        d1.setSalario(2000.00);
+
+        ControleBonificacao controleBonificacao = new ControleBonificacao();
+        controleBonificacao.registra(g1);
+        controleBonificacao.registra(f1);
+        controleBonificacao.registra(ev1);
+        controleBonificacao.registra(d1);
+
+        System.out.println(controleBonificacao.getSoma());
+        
+    }
+}
---
+++ bytebank-herdado/Teste.java
@@ -0,0 +1,11 @@
+public class Teste {
+    public static void main(String[] args) {
+        
+        Veiculo m = new Moto();
+        m.liga();
+
+        Veiculo c = new Carro();
+        c.liga();
+    
+    }
+}
