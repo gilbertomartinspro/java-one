6f1a5a904b3f981cd641fab17715f875960415c7
Gilberto Martins <gilbertomartinspro@gmail.com>
2023-03-17 15:41:23 -0300

    Praticando herança e interfaces

--- bytebank-herdado/Gerente.java
+++ bytebank-herdado/Gerente.java
@@ -1,20 +1,22 @@
 // Gerente é um Funcionario, herda da classe Funcionario, assina o contrato Autenticavel
 public class Gerente extends Funcionario implements Autenticavel {
-    private int senha;
+
+    private AutenticacaoUtil autenticador;
+
+    public Gerente() {
+        this.autenticador = new AutenticacaoUtil();
+    }
 
     @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha) {
-        if(this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
+       return this.autenticador.autentica(senha);
     }
+    
     public double getBonificacao() {
         return super.getSalario();
     }
